b44320a3351ae9c608241b6ef0848c80294d6ea9
tan <laosomboon@gmail.com>
2017-10-10 13:10:25 -0400

    version 1.0

---
+++ js/fetchdata.js
@@ -0,0 +1,68 @@
+function createMenuElement(ficon, menuName, menuPrice, menuDesc, menuImg) {
+    if (!menuPrice) {
+        return null;
+    }
+    var div = document.createElement('div');
+    div.setAttribute("class", "mix col-xs-12 col-sm-12 col-md-6 menu-restaurant");
+    div.classList.add(ficon);
+    div.setAttribute('data-cat', ficon);
+
+
+    var spanWrapper = document.createElement('span');
+    spanWrapper.classList.add('clearfix');
+
+    var a1 = document.createElement('a');
+    a1.setAttribute("class", "menu-title");
+    a1.setAttribute("data-meal-img", menuImg);
+    a1.innerHTML = menuName;
+
+    var lineSpan = document.createElement('span');
+    lineSpan.setAttribute("style", "left:260px; right: 44px; color:black;");
+    lineSpan.setAttribute("class", "menu-line");
+
+    var priceSpan = document.createElement('span');
+    priceSpan.setAttribute("style", "float:right;");
+    priceSpan.setAttribute("class", "menu-price");
+    priceSpan.innerHTML = menuPrice;
+
+
+    var subtitleSpan = document.createElement("span");
+    subtitleSpan.setAttribute("class", "menu-subtitle");
+    subtitleSpan.innerHTML = menuDesc;
+
+    spanWrapper.appendChild(a1);
+    spanWrapper.appendChild(lineSpan);
+    spanWrapper.appendChild(priceSpan);
+
+    div.appendChild(spanWrapper);
+    div.appendChild(subtitleSpan);
+
+    return div;
+}
+
+
+/*Initialize menus*/
+var allmenus;
+
+/*Fetch Menu from Firebase*/
+
+const dbRefObject = firebase.database().ref().child('allmenus');
+
+
+dbRefObject.on('value', function(snap) {
+    allmenus = snap.val();
+    allmenus.forEach(function(c) {
+
+        var menus = c.menus;
+
+        menus.forEach(function(menuItem) {
+
+            var elem = createMenuElement(c.icon, menuItem.name, menuItem.burlington, menuItem.description, menuItem.image);
+            var menuContainer = document.getElementById('Container');
+            if (elem) {
+                menuContainer.appendChild(elem);
+            }
+        });
+    });
+
+}); /*end on snapshot*/
