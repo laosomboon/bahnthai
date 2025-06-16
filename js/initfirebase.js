b1d599077cb3ed0b9b78a09674e9a042817c9a9f
prawat laosomboon <tan@Tan-Macbook-Pro.local>
2025-06-16 18:52:54 -0400

    fix firebase setting

--- js/initfirebase.js
+++ js/initfirebase.js
@@ -15,7 +15,18 @@ import {
   addDoc,
   updateDoc,
 } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js";
-import { firebaseConfig } from "./firebaseConfig.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFGsDVtTUs6_nB8nfaW5EhceJ7BlE3_F4",
+  authDomain: "bahnthai-2ea23.firebaseapp.com",
+  projectId: "bahnthai-2ea23",
+  storageBucket: "bahnthai-2ea23.firebasestorage.app",
+  messagingSenderId: "134991899936",
+  appId: "1:134991899936:web:525833efd42d3f36b83b45",
+  measurementId: "G-Z3L1GBGT5B"
+};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
